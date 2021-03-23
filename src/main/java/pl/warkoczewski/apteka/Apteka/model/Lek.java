34b1b89b7528cea2ecc9b2f5c9e8e207e0ad7e4a
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-03-23 09:00:49 +0100

    refactored Leki , fields expiresAt and amount added

--- src/main/java/pl/warkoczewski/apteka/Apteka/model/Lek.java
+++ src/main/java/pl/warkoczewski/apteka/Apteka/model/Lek.java
@@ -1,14 +1,22 @@
 package pl.warkoczewski.apteka.Apteka.model;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Lek {
 
     private Long id;
     private String name;
+    private LocalDate expiresAt;
+    private Long amount;
 
-    public Lek(Long id, String name) {
+
+    public Lek(Long id, String name, LocalDate expiresAt, Long amount) {
         this.id = id;
         this.name = name;
+        this.expiresAt = expiresAt;
+        this.amount = amount;
     }
 
     public Lek() {
@@ -28,6 +36,21 @@ public class Lek {
 
     public void setName(String name) {
         this.name = name;
+    }
+    public LocalDate getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDate expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
     }
 
 }
--- src/main/java/pl/warkoczewski/apteka/Apteka/repository/LekRepositoryImpl.java
+++ src/main/java/pl/warkoczewski/apteka/Apteka/repository/LekRepositoryImpl.java
@@ -3,6 +3,8 @@ package pl.warkoczewski.apteka.Apteka.repository;
 import org.springframework.stereotype.Repository;
 import pl.warkoczewski.apteka.Apteka.model.Lek;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +19,9 @@ public class LekRepositoryImpl implements LekRepository {
     }
 
     private void initialize() {
-        leki.add(new Lek(1l, "Apap"));
-        leki.add(new Lek(2l, "Nurofen"));
-        leki.add(new Lek(3l, "VitC"));
-
+        leki.add(new Lek(1l, "Apap", LocalDate.of(2021, 12, 12), 20l));
+        leki.add(new Lek(2l, "Nurofen", LocalDate.of(2021, 12, 13), 30l));
+        leki.add(new Lek(3l, "VitC", LocalDate.of(2030, 01, 01), 20l));
     }
 
     @Override
